52e688b5d1605b9ac80cb613db7c9a501848c817
xrole <xrolediamond@gmail.com>
2022-07-18 13:32:13 +0100

    refactor http client get function

--- lib/app/core/client/http_client.dart
+++ lib/app/core/client/http_client.dart
@@ -3,14 +3,12 @@ import 'package:dio/dio.dart';
 class HttpClient {
   final Dio dio = Dio();
 
-  Future<dynamic> get(String route) async {
-    Future<dynamic> get(String url) async {
-      try {
-        final response = await dio.get(url);
-        return response;
-      } catch (e) {
-        print('Dio caught error ====================> $e');
-      }
+  Future<dynamic> get(String url) async {
+    try {
+      final response = await dio.get(url);
+      return response;
+    } catch (e) {
+      print('Dio caught error ====================> $e');
     }
   }
 }
